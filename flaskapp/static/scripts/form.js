d3d6f2eb6c1dd3c99ba4b0ac34c269c066179262
azertypow <azertypow@gmail.com>
2019-05-03 23:55:23 +0200

    refactoring

--- flaskapp/static/scripts/form.js
+++ flaskapp/static/scripts/form.js
@@ -1,44 +1,45 @@
 class TextAreaContentChangeListener {
 
-  _userIdValue = "";
+    _userIdValue = "";
+    textAreaHTMLElement;
 
-  constructor(textAreaQuerySelector) {
+    constructor(textAreaQuerySelector) {
 
-    if (!window.fetch) console.error("no fetch support in this browser");
+        if (!window.fetch) console.error("no fetch support in this browser");
 
-    this.userId = document.querySelector(textAreaQuerySelector);
+        this.textAreaHTMLElement = document.querySelector(textAreaQuerySelector);
 
-    this.userId.addEventListener("keyup", () => {
-      const new_userIdValue = this.userId.value;
+        this.textAreaHTMLElement.addEventListener("keyup", () => {
+            const new_userIdValue = this.textAreaHTMLElement.value;
 
-      if (this._userIdValue !== new_userIdValue) this.userIdValue = new_userIdValue
-    });
-  }
+            if (this._userIdValue !== new_userIdValue) this.userIdValue = new_userIdValue
+        });
+    }
 
-  set userIdValue(value) {
-    this._userIdValue = value;
-    this.sendNewUserIdValue()
-  }
+    set userIdValue(value) {
+        this._userIdValue = value;
+        this._sendNewUserIdValue()
+    }
 
-  sendNewUserIdValue() {
+    _sendNewUserIdValue() {
 
-    const valueToSend = this._userIdValue;
+        const valueToSend = this._userIdValue;
 
-    window.fetch("/textsentiment", {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        value: valueToSend
-      })
-    }).then(value => {
-      return value.json().then(value1 => {
-        console.info(value1)
-      })
-    })
-  }
+        window.fetch("/textsentiment", {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                value: valueToSend
+            })
+        }).then(value => {
+            return value.json().then(value1 => {
+                console.info(value1)
+            })
+        })
+    }
 }
 
 new TextAreaContentChangeListener("#user_input");
