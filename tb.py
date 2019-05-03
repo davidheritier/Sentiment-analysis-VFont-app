62f50604786cfa69b92240a9bc080454ab253897
David Héritier <heritier.david@gmail.com>
2019-05-03 08:28:31 +0200

    Font update

--- tb.py
+++ tb.py
@@ -1,5 +1,20 @@
 from textblob import TextBlob
 
-txt = TextBlob("Hello World. It's good to see you. Thanks for buying this book.")
 
-print(txt.sentiment.polarity, txt.sentiment.subjectivity, txt.sentiment.polarity * txt.sentiment.subjectivity)
+file = open('tb-results.txt','a')
+
+
+txt = "Hello I am very happy to see you are you pleased?"
+tb = TextBlob(txt)
+pol = str(tb.sentiment.polarity)
+sub = str(tb.sentiment.subjectivity)
+mul = str(tb.sentiment.polarity * tb.sentiment.subjectivity)
+
+
+file.write(txt + '\r')
+file.write('polarity: ' + pol)
+file.write('\tsubjectivity: ' + sub)
+file.write('\tpolarity * subjectivity: ' + mul)
+file.write('\r\r')
+
+print('polarity: ' + pol + 'subjectivity: ' + sub + 'multiply: ' mul)
