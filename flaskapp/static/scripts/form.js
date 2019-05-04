232fd7ac66458b8393c31b1ecde7e2fcc57d5705
David Héritier <heritier.david@gmail.com>
2019-05-04 12:03:30 +0200

    Modified variation values

--- flaskapp/static/scripts/form.js
+++ flaskapp/static/scripts/form.js
@@ -1,64 +1,64 @@
 class TextareaContentChangeListener {
 
-    _textAreaValue = "";
-    textAreaHTMLElement;
+  _textAreaValue = "";
+  textAreaHTMLElement;
 
-    constructor(textareaQuerySelector) {
+  constructor(textareaQuerySelector) {
 
-        if (!window.fetch) console.error("no fetch support in this browser");
+    if (!window.fetch) console.error("no fetch support in this browser");
 
-        this.textAreaHTMLElement = document.querySelector(textareaQuerySelector);
+    this.textAreaHTMLElement = document.querySelector(textareaQuerySelector);
 
-        this.textAreaHTMLElement.addEventListener("keyup", () => {
-            const newTextAreaValue = this.textAreaHTMLElement.value;
+    this.textAreaHTMLElement.addEventListener("keyup", () => {
+      const newTextAreaValue = this.textAreaHTMLElement.value;
 
-            if (this._textAreaValue !== newTextAreaValue) this.textAreaValue = newTextAreaValue
-        });
-    }
+      if (this._textAreaValue !== newTextAreaValue) this.textAreaValue = newTextAreaValue
+    });
+  }
 
-    set textAreaValue(value) {
-        this._textAreaValue = value;
-        this._sendNewTextareaValue()
-    }
+  set textAreaValue(value) {
+    this._textAreaValue = value;
+    this._sendNewTextareaValue()
+  }
 
-    _sendNewTextareaValue() {
+  _sendNewTextareaValue() {
 
-        const valueToSend = this._textAreaValue;
+    const valueToSend = this._textAreaValue;
 
-        window.fetch("/textsentiment", {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                value: valueToSend
-            })
-        }).then(valueFromFlaskApp => {
-            return valueFromFlaskApp.json().then(JSONFromValueFromFlaskApp => {
-                console.info(JSONFromValueFromFlaskApp);
+    window.fetch("/textsentiment", {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        value: valueToSend
+      })
+    }).then(valueFromFlaskApp => {
+      return valueFromFlaskApp.json().then(JSONFromValueFromFlaskApp => {
+        console.info(JSONFromValueFromFlaskApp);
 
-                this._setCssFontVariablePropertyOfTextarea(JSONFromValueFromFlaskApp)
-            })
-        })
-    }
+        this._setCssFontVariablePropertyOfTextarea(JSONFromValueFromFlaskApp)
+      })
+    })
+  }
 
-    _setCssFontVariablePropertyOfTextarea(JSONSentimentAnalyse) {
+  _setCssFontVariablePropertyOfTextarea(JSONSentimentAnalyse) {
 
-        const polarity      = JSONSentimentAnalyse.polarity;
-        const subjectivity  = JSONSentimentAnalyse.subjectivity;
+    const polarity = JSONSentimentAnalyse.polarity;
+    const subjectivity = JSONSentimentAnalyse.subjectivity;
 
-        //  weight = str(int((tb.sentiment.polarity * 100) * (tb.sentiment.subjectivity * 100) / 10))
-        const value1 = (polarity * 100) * (subjectivity * 100) / 100;
+    // weight = str(int((tb.sentiment.polarity * 100) * (tb.sentiment.subjectivity * 100) / 10))
+    const value1 = 400 + ((polarity * 100) * (subjectivity * 100) / 10);
 
-        //  contrast = str(int(500 + (tb.sentiment.subjectivity / 2 * 1000)))
-        const value2 = 500 + (subjectivity / 2 * 1000);
+    // contrast = str(int(500 + (tb.sentiment.subjectivity / 2 * 1000)))
+    const value2 = 500 - ((subjectivity * 1000) / 2);
 
-        //  slant = str(int(tb.sentiment.polarity * 1000))
-        const value3 = polarity * 1000;
+    // slant = str(int(tb.sentiment.polarity * 1000))
+    const value3 = 500 - ((polarity * 1000) / 2);
 
-        this.textAreaHTMLElement.style.fontVariationSettings = `'wght'${value1}, 'CNTR'${value2}, 'slnt'${value3}`;
-    }
+    this.textAreaHTMLElement.style.fontVariationSettings = `'wght'${value1}, 'CNTR'${value2}, 'slnt'${value3}`;
+  }
 }
 
 new TextareaContentChangeListener("#user_input");
