66812292a01fc901ca83cf5ae2e54628c8c386cf
David Héritier <heritier.david@gmail.com>
2019-05-08 13:36:55 +0200

    Database reinitialized

--- flaskapp/model.py
+++ flaskapp/model.py
@@ -3,9 +3,9 @@ from flaskapp import db
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     txt = db.Column(db.Text, nullable=False)
-    # value1 = db.Column(db.Float, nullable=False)
-    # value2 = db.Column(db.Float, nullable=False)
-    # value3 = db.Column(db.Float, nullable=False)
+    value1 = db.Column(db.Float, nullable=False)
+    value2 = db.Column(db.Float, nullable=False)
+    value3 = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         return f"Entry('{self.txt}')"
