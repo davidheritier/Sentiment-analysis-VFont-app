81f45dd6ee398bbf8cfdd602d3a95a00ee6cc79a
David Héritier <heritier.david@gmail.com>
2019-06-05 15:16:19 +0200

    Minor changes

--- flaskapp/form.py
+++ flaskapp/form.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class EntryForm(FlaskForm):
     user_input = TextAreaField('Enter your text', validators=[DataRequired()])
-    submit = SubmitField('Save')
+    submit = SubmitField('Save to database')
 
 class GetFont(FlaskForm):
     submit = SubmitField('Download font')
